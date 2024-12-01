bd578eb5f921b5940eda6b88ae5ffa0149d0c8c8
Tamer Hamad Faour <tamer@tameronline.com>
2024-12-01 06:13:05 +0100

    run 02

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,9 +1,16 @@
 import streamlit as st
+import requests
 
 st.title("Streamlit Application")
 
 st.write("This is a simple Streamlit app running in a Docker container.")
 
-user_name = st.text_input("Enter your name:")
-if user_name:
-    st.write(f"Hello, {user_name}!")
+name = st.text_input("Enter a name:")
+
+# إذا كان هناك اسم مدخل من قبل المستخدم، إرسال البيانات إلى FastAPI
+if name:
+    response = requests.post("http://fastapi:8000/add_name", json={"name": name})
+    if response.status_code == 200:
+        st.write(f"Name received: {response.json()['name_received']}")
+    else:
+        st.write("Error sending data to FastAPI")
--- main/fastapi_app.py
+++ main/fastapi_app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class NameRequest(BaseModel):
+    name: str
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.post("/add_name")
+async def add_name(data: NameRequest):
+     return {"name_received": data.name}
