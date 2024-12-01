2019f19fc3c43b7e0fe19740dfdce5e85fdbfbd3
Tamer Hamad Faour <tamer@tameronline.com>
2024-12-01 16:43:17 +0100

    Make

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	docker-compose -p pro_mystro_01 up --build
+
+up:
+	docker-compose -p pro_mystro_01 up
+
+down:
+	docker-compose -p pro_mystro_01 down
--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -7,10 +7,15 @@ st.write("This is a simple Streamlit app running in a Docker container.")
 
 name = st.text_input("Enter a name:")
 
-# إذا كان هناك اسم مدخل من قبل المستخدم، إرسال البيانات إلى FastAPI
 if name:
-    response = requests.post("http://fastapi:8000/add_name", json={"name": name})
-    if response.status_code == 200:
-        st.write(f"Name received: {response.json()['name_received']}")
-    else:
-        st.write("Error sending data to FastAPI")
+    try:
+
+        response = requests.post("http://fastapi:8000/add_name", json={"name": name})
+        if response.status_code == 200:
+            st.write(f"Name received: {response.json()['name_received']}")
+        else:
+            st.write("Error sending data to FastAPI")
+    except requests as e:
+        st.write(f"Err: {e}")
+
+
